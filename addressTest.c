98f01cda5afdb7c23232666a775cfe3798d71091
lleaff <lleaff@users.noreply.github.com>
2014-12-19 13:15:53 +0100

    update

--- addressTest.c
+++ addressTest.c
@@ -2,7 +2,9 @@
 
 int main() {
 	int victim = 15;
+	printf("int victim = 15;\nvictimAddress =\t%p\n", &victim);
 	int mark = 6;
+	printf("int mark = 6;\nmarkAddress =\t%p\n", &mark);
 
 	int *bullet = &mark;
 	
@@ -10,17 +12,17 @@ int main() {
 	bullet -= 1;
 	printf("bullet -= 1\n");
 
-	printf("victim=%d\tmark=%d\tbulletTarget=%d\tbulletTargetAddress=%p\n", victim, mark, *bullet, bullet);
+	printf("victim=%d\tmark=%d\tbulletTarget=%d\tbulletTargetAddress = %p\n", victim, mark, *bullet, bullet);
 
 	//wind
 	bullet += 1;
 	printf("bullet += 1\n");
 
-	printf("victim=%d\tmark=%d\tbulletTarget=%d\tbulletTargetAddress=%p\n", victim, mark, *bullet, bullet);
+	printf("victim=%d\tmark=%d\tbulletTarget=%d\t\tbulletTargetAddress = %p\n", victim, mark, *bullet, bullet);
 
 	//wind
 	bullet += 1;
 	printf("bullet += 1\n");
 
-	printf("victim=%d\tmark=%d\tbulletTarget=%d\tbulletTargetAddress=%p\n", victim, mark, *bullet, bullet);
+	printf("victim=%d\tmark=%d\tbulletTarget=%d\t\tbulletTargetAddress = %p\n", victim, mark, *bullet, bullet);
 }
