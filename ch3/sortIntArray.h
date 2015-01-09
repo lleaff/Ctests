0ebcefa1dd30fd2a04e018ed325076a2d34482a6
lleaff <lleaff@users.noreply.github.com>
2015-01-09 15:11:50 +0100

    done & tested

---
+++ ch3/sortIntArray.h
@@ -0,0 +1,12 @@
+#ifndef SORTINTARRAY_H
+#define SORTINTARRAY_H
+
+#define MAXINTARRLENGTH 500
+
+void printIntArrCommaSeparated(int arrayLength, int array[]);
+
+int sortIntArray(int arrayLength, int array[]);
+
+typedef enum { YES, NO, ERROR } ReturnCode;
+
+#endif
