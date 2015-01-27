977dfcd81a21c4a421ba2e82cb54df006a7d5773
lleaff <lleaff@users.noreply.github.com>
2015-01-27 23:55:42 +0100

    support for standard input

--- ch4/searchChar.c
+++ ch4/searchChar.c
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <string.h>
 
+#define MAXSTRINGSIZE 1000
 #define OUTBUFFERWIDTH 78
 
 typedef enum { FALSE, TRUE } BOOL;
@@ -28,7 +29,11 @@ int main(int argc, char **argv)
 {
 	BOOL test = FALSE;
 	int testC;
+	char searchedChar;
+	char containingString[MAXSTRINGSIZE];
 	if (argc == 1) {
+		scanf("%c", &searchedChar);
+		scanf("%s", containingString);
 	} else if (argc <= 3) {
 		if (!strcmp(argv[1], "--test")) {
 			test = TRUE;
@@ -38,6 +43,8 @@ int main(int argc, char **argv)
 			}
 		}
 	}
+
+	printf("%d", searchChar(searchedChar, containingString));
 
 	if (test) {
 		char testString[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
