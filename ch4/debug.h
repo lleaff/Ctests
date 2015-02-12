2ac2bc58ae9dc2213a5292583dec3dfa53c560c5
lleaff <lleaff@users.noreply.github.com>
2015-02-12 07:42:53 +0100

    forgot include guard

--- ch4/debug.h
+++ ch4/debug.h
@@ -1,10 +1,11 @@
+#ifndef DEBUG_H
+#define DEBUG_H
 //Put "#define DEBUG" at the top of your file to enable the functions
 #include <stdio.h>
-
 #ifdef DEBUG
 #define DEBUGPRINT(...)		fprintf(stderr, __VA_ARGS__)
 #define DEBUGP(...)			DEBUGPRINT(__VA_ARGS__)
 #else
 #define DEBUGPRINT(...)
 #define DEBUGP(...)
-#endif /* DEBUG */
+#endif /* DEBUG_H, DEBUG */
