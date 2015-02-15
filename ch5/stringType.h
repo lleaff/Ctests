fb2e5e01406140f9a23827105bf20a0936107d98
lleaff <lleaff@users.noreply.github.com>
2015-02-15 19:39:06 +0100

    added compareStrings to header

--- ch5/stringType.h
+++ ch5/stringType.h
@@ -11,12 +11,12 @@ typedef struct String {
 	int LengthLimit;
 } String;
 
-int isStringType(void *ptr);
+String newString(int length, char *cStyleString);
 
+int isStringType(void *ptr);
 #define strlen(string) String__strlength(&(string))
 int String__strlength(void *ptr);
-
-String newString(int length, char *cStyleString);
 int CopyString(String destination, void *origin);
+int compareStrings(String str1, String str2);
 
 #endif /*  STRINGTYPE_H */
