250f08a4629169f60f78ac142a5b182ba77e275a
lleaff <lleaff@users.noreply.github.com>
2015-02-15 19:37:32 +0100

    update

--- ch5/stringType.h
+++ ch5/stringType.h
@@ -3,7 +3,6 @@
 #include <stdlib.h> //For NULL pointer identifier
 #include <string.h>
 
-#define strlen(string) String__strlength(&(string))
 
 typedef struct String {
 	double stringTypeID;
@@ -12,8 +11,12 @@ typedef struct String {
 	int LengthLimit;
 } String;
 
+int isStringType(void *ptr);
+
+#define strlen(string) String__strlength(&(string))
+int String__strlength(void *ptr);
+
 String newString(int length, char *cStyleString);
 int CopyString(String destination, void *origin);
-int isStringType(void *ptr);
 
 #endif /*  STRINGTYPE_H */
