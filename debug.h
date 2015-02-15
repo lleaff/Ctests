96665570c73d96346a9035f2dbd9056aef552704
lleaff <lleaff@users.noreply.github.com>
2015-02-15 21:58:52 +0100

    DEBUGSTATEMENT macro, unsafe

--- debug.h
+++ debug.h
@@ -5,8 +5,10 @@
 #ifdef DEBUG
 #define DEBUGPRINT(...)		fprintf(stderr, __VA_ARGS__)
 #define DEBUGP(...)			DEBUGPRINT(__VA_ARGS__)
+#define DEBUGSTATEMENT(...)	__VA_ARGS__
 #else
 #define DEBUGPRINT(...)
 #define DEBUGP(...)
+#define DEBUGSTATEMENT(...)
 #endif /* DEBUG */
 #endif /* DEBUG_H */
