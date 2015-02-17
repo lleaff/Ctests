df4ba50dd749efa1d89842bc363cb25078025412
lleaff <lleaff@users.noreply.github.com>
2015-02-17 18:10:04 +0100

    String__strncpy done & tested, simplified String__strncat too

--- ch5/stringnfuncs.c
+++ ch5/stringnfuncs.c
@@ -11,8 +11,10 @@
 int String__strncat(void *str1, void *str2, int n)
 {
 	int l1 = strlen(*(void **)str1), l2 = strlen(*(void **)str2);
+	char *string1Chars = getStringChars(str1);
 	char *string2Chars = getStringChars(str2);
 	int returnVal = 0;
+	int newLength = l1 + MIN(n, l2);
 	if (isStringType((void **)str1)) {
 		String string1 = *(String *)str1;
 		if (!((string1.LengthLimit <= l1 + n) && 
@@ -21,25 +23,46 @@ int String__strncat(void *str1, void *str2, int n)
 			/*  Return the number of extra characters */
 			returnVal = n - (MAX(string1.LengthLimit, strlen(str2)));
 		}
-		int newLength = l1 + MIN(n, l2);
-		memcpy(string1.Chars + l1, string2Chars, (newLength - l1) * sizeof(char));
 		string1.Length = newLength;
 		string1.Chars[newLength] = '\0';
 	} else {
 		if (l1 <= l1 + MIN(l2, n)) {
-			strcpy(str1 + l1, string2Chars);
 		} else {
 			fprintf(stderr, "ERROR: stringnfuncs.c:String__strncat: destination string too short\n");
 			returnVal = l1 - MIN(l2, n); //Failure, result < 0
+			return returnVal;
 		}
 	}
+	memcpy(string1Chars + l1, string2Chars, (newLength - l1) * sizeof(char));
 	return returnVal;
 }
 
 int String__strncpy(void *str1, void *str2, int n)
 {
-
-	return 0;
+	int l1 = strlen(*(void **)str1), l2 = strlen(*(void **)str2);
+	char *string1Chars = getStringChars(str1);
+	char *string2Chars = getStringChars(str2);
+	int returnVal = 0;
+	if (isStringType((void **)str1)) {
+		String string1 = *(String *)str1;
+		if (!(string1.LengthLimit >= MIN(n, l2))) {
+			changeStringLength(string1, MIN(n, l2));
+			/*  Return the number of extra characters */
+			returnVal = MAX(n, l2);
+		}
+		int newLength = MIN(n, l2);
+		string1.Length = newLength;
+		string1Chars[newLength] = '\0';
+	} else {
+		if (l1 <= l1 + MIN(l2, n)) {
+		} else {
+			fprintf(stderr, "ERROR: stringnfuncs.c:String__strncpy: destination string too short\n");
+			returnVal = l1 - MIN(l2, n); //Failure, result < 0
+			return returnVal;
+		}
+	}
+	memcpy(string1Chars, string2Chars, (MIN(n, l2)) * sizeof(char));
+	return returnVal;
 }
 
 
@@ -55,20 +78,22 @@ int randInt(int lo, int up)
 int main()
 {
 	String teststr1 = newString(50, "Hello...");
-	String teststr2 = newString(50, "Goodbye");
+	String teststr2 = newString(50, "Goodbye Mister");
 	srand((int)time(NULL));
-	printf("str1: %s\nstr2: %s\n", teststr1.Chars, teststr2.Chars);
 	int n;
-
+	printf("\n");
+	printf("str1: %s\nstr2: %s\n", teststr1.Chars, teststr2.Chars);
 	n = randInt(0, strlen(teststr2));
-	printf("\nCopy the first %d characters from str2 at the end of str1...\n\n", n);
+	printf("--- Copy the first %d characters from str2 at the end of str1...\n", n);
 	strncat(teststr1, teststr2, n);
 	printf("str1: %s\nstr2: %s\n", teststr1.Chars, teststr2.Chars);
-
+	printf("\n");
 	n = randInt(0, strlen(teststr2));
-	printf("\nCopy the first %d characters from str2 on str1...\n\n", n);
-	strncpy(teststr1, teststr2, n);
-	printf("str1: %s\nstr2: %s\n", teststr1.Chars, teststr2.Chars);
+	String teststr3 = newString(50, "Hello...");
+	printf("str3: %s\nstr2: %s\n", teststr3.Chars, teststr2.Chars);
+	printf("--- Copy the first %d characters from str2 on str3...\n", n);
+	strncpy(teststr3, teststr2, n);
+	printf("str3: %s\nstr2: %s\n", teststr3.Chars, teststr2.Chars);
 
 
 	return 0;
--- ch5/stringnfuncs.h
+++ ch5/stringnfuncs.h
@@ -7,6 +7,6 @@ int String__strncat(void *str1, void *str2, int n);
 #define strncat(str1, str2, n)	String__strncat(&(str1), &(str2), n)
 
 int String__strncpy(void *str1, void *str2, int n);
-#define strncpy(str1, str2, n)	String__strncat(&(str1), &(str2), n)
+#define strncpy(str1, str2, n)	String__strncpy(&(str1), &(str2), n)
 
 #endif /* STRINGNFUNCS_H */
