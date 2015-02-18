5f2cee31d2c5a3e29e22fdcda8d0ae2ee05e7952
lleaff <lleaff@users.noreply.github.com>
2015-02-18 21:18:04 +0100

    formatting

--- structScratchpad.c
+++ structScratchpad.c
@@ -12,9 +12,14 @@ int main()
 
 	struct house cabin;
 
-	printf("%-20s = %-2i  %-23s = %-2i\n", "sizeof(cabin.horse)",		(int)sizeof(cabin.horse),	"offsetof(house, horse)",	(int)offsetof(house, horse));
-	printf("%-20s = %-2i  %-23s = %-2i\n", "sizeof(cabin.rabbit)",	(int)sizeof(cabin.rabbit),	"offsetof(house, rabbit)",	(int)offsetof(house, rabbit));
-	printf("%-20s = %-2i  %-23s = %-2i\n", "sizeof(cabin.cat)",		(int)sizeof(cabin.cat),		"offsetof(house, cat)",		(int)offsetof(house, cat));
-	printf("%-20s = %-2i  %-23s = %-2i\n", "sizeof(cabin.pig)",		(int)sizeof(cabin.pig),		"offsetof(house, pig)",		(int)offsetof(house, pig));
+	printf("%-20s = %-2i  %-23s = %-2i\n", "sizeof(cabin.horse)",	(int)sizeof(cabin.horse),
+			"offsetof(house, horse)",	(int)offsetof(house, horse));
+	printf("%-20s = %-2i  %-23s = %-2i\n", "sizeof(cabin.rabbit)",	(int)sizeof(cabin.rabbit),
+			"offsetof(house, rabbit)",	(int)offsetof(house, rabbit));
+	printf("%-20s = %-2i  %-23s = %-2i\n", "sizeof(cabin.cat)",		(int)sizeof(cabin.cat),	
+			"offsetof(house, cat)",		(int)offsetof(house, cat));
+	printf("%-20s = %-2i  %-23s = %-2i\n", "sizeof(cabin.pig)",		(int)sizeof(cabin.pig),	
+			"offsetof(house, pig)",		(int)offsetof(house, pig));
 
+	return 0;
 }
