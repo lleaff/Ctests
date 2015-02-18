262830db15f92e2d0da2c090c34db98a13294201
lleaff <lleaff@users.noreply.github.com>
2015-02-18 21:48:08 +0100

    messy

--- structScratchpad.c
+++ structScratchpad.c
@@ -10,6 +10,7 @@ int main()
 		int cat;
 		char rabbit;
 		float pig;
+		char rabbit2;
 	} house;
 
 	struct house myHouse;
@@ -18,11 +19,14 @@ double horse;\n\
 int cat;\n\
 char rabbit;\n\
 float pig;\n\
+char rabbit2;\n\
 \n");
 	printf("%-22s = %-2i  %-23s = %-2i\n", "sizeof(myHouse.horse)",	(int)sizeof(myHouse.horse),
 			"offsetof(house, horse)",	(int)offsetof(house, horse));
 	printf("%-22s = %-2i  %-23s = %-2i\n", "sizeof(myHouse.rabbit)",	(int)sizeof(myHouse.rabbit),
 			"offsetof(house, rabbit)",	(int)offsetof(house, rabbit));
+	printf("%-22s = %-2i  %-23s = %-2i\n", "sizeof(myHouse.rabbit2)",	(int)sizeof(myHouse.rabbit2),
+			"offsetof(house, rabbit2)",	(int)offsetof(house, rabbit2));
 	printf("%-22s = %-2i  %-23s = %-2i\n", "sizeof(myHouse.cat)",		(int)sizeof(myHouse.cat),	
 			"offsetof(house, cat)",		(int)offsetof(house, cat));
 	printf("%-22s = %-2i  %-23s = %-2i\n", "sizeof(myHouse.pig)",		(int)sizeof(myHouse.pig),	
@@ -33,10 +37,17 @@ float pig;\n\
 
 	typedef struct cabin {
 		int cat;
-		char rabbit;
-		char rabbit2;
+		char rabbits[8];
 		float pig;
+		char mouse1;
+		char mouse2;
+		char mouse3;
 		double horse;
+		char mouse4;
+		char mouse5;
+		char mouse6;
+		char mouse7;
+		char mouse8;
 	} cabin;
 
 	struct cabin myCabin;
@@ -50,8 +61,8 @@ double horse;\n\
 \n");
 	printf("%-22s = %-2i  %-23s = %-2i\n", "sizeof(myCabin.horse)",	(int)sizeof(myCabin.horse),
 			"offsetof(cabin, horse)",	(int)offsetof(cabin, horse));
-	printf("%-22s = %-2i  %-23s = %-2i\n", "sizeof(myCabin.rabbit)",	(int)sizeof(myCabin.rabbit),
-			"offsetof(cabin, rabbit)",	(int)offsetof(cabin, rabbit));
+	printf("%-22s = %-2i  %-23s = %-2i\n", "sizeof(myCabin.rabbit)",	(int)sizeof(myCabin.rabbits),
+			"offsetof(cabin, rabbit)",	(int)offsetof(cabin, rabbits));
 	printf("%-22s = %-2i  %-23s = %-2i\n", "sizeof(myCabin.cat)",		(int)sizeof(myCabin.cat),	
 			"offsetof(cabin, cat)",		(int)offsetof(cabin, cat));
 	printf("%-22s = %-2i  %-23s = %-2i\n", "sizeof(myCabin.pig)",		(int)sizeof(myCabin.pig),	
