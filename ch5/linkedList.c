1be4921259d3e563ee1ee71fd7a5406ba8b93696
lleaff <lleaff@users.noreply.github.com>
2015-02-22 20:24:44 +0100

    done type and size init, uncomplete

--- ch5/linkedList.c
+++ ch5/linkedList.c
@@ -9,14 +9,19 @@ typedef struct LL {
 	char elem[];
 } LL;
 
-#define newLL(firstElem, ...)	LL__newLL(&(firstElem), __VA_ARGS__)
+void resolveTypeAndSizeFromString(char* myString, TYPE* type, int* size);
 
-LL LL__newLL(int size, ...)
+#define newLL(typeOrSize, ...)	LL__newLL(#typeOrSize, __VA_ARGS__)
+
+LL LL__newLL(char* typeOrSize, ...)
 {
+	TYPE type;
+	int size;
+	resolveTypeAndSizeFromString(typeOrSize, &type, &size);
+
 	va_list ap;
-	va_start(ap, size);
+	va_start(ap, typeOrSize);
 	char elemmem[size];
-	TYPE type = getType(size);
 	switch (type) {
 		case CHAR:		*elemmem = va_arg(ap, char);
 					break;
@@ -35,6 +40,47 @@ LL LL__newLL(int size, ...)
 		default:	break;
 	}
 
+}
+
+//Returns -1 if the char doesn't represent a digit
+int tryGetIntFromChar(char digit)
+{
+	if (digit >= '0' && digit <= '9') {
+		return digit - '0'; 
+	} else {
+		return -1; //Not a digit
+	}
+}
+
+//Returns 1 if successful, 0 if string doesn't convert to an int
+int tryGetIntFromString(char* myString, int* convertedInt)
+{
+	*convertedInt = 0;
+	int sign = 1;
+	if (*myString == '-') {
+		sign = -1;
+		++myString;
+	}
+	int digit;
+	for (; *myString != '\0'; myString++) {
+		if ((digit = tryGetIntFromChar(*myString)) >= 0) {
+			*convertedInt += digit;
+		} else {
+			return 0; //Error string doesn't convert to an int
+		}
+	}
+	*convertedInt *= sign;
+	return 1; //Success
+}
+
+void resolveTypeAndSizeFromString(char* myString, TYPE* type, int* size)
+{
+	if (tryGetIntFromString(myString, size)) {	/*	String is int	*/
+		*type = getTypeFromSize(*size);
+	} else {									/*	String is type	*/
+		*type = getTypeFromString(myString);
+		*size = SIZEOF(*type);
+	}
 }
 
 int main()
