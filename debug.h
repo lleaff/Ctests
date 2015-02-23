22c75d756f00b2b641986afc5a0a6f5bb053f991
lleaff <lleaff@users.noreply.github.com>
2015-02-24 00:16:04 +0100

    compact

--- debug.h
+++ debug.h
@@ -8,23 +8,19 @@
 #define DEBUGP(...)			DEBUGPRINT(__VA_ARGS__)
 #define DEBUGSTATEMENT(...)	__VA_ARGS__
 #define DEBUGS(...)	DEBUGSTATEMENT(__VA_ARGS__)
-
 #define PUTCHARLIT(myChar)	do{switch (myChar) {\
-case '\t':printf("\\t");break;\
-case '\n':printf("\\n");break;\
-case '\0':printf("\\0");break;\
-case ' ':printf("_space_");break;\
-default:putchar(myChar);break;}\
-}while(0);
-
+	case '\t':printf("\\t");break;\
+	case '\n':printf("\\n");break;\
+	case '\0':printf("\\0");break;\
+	case ' ':printf("_space_");break;\
+	default:putchar(myChar);break;}\
+	}while(0);
 #define TOLOWER(myString)	DEBUG__TOLOWER(myString)
-char* DEBUG__TOLOWER(char* myString)
-{ int i; for(i=0;myString[i]!='\0';++i){myString[i]=tolower(myString[i]);} return myString; }
-
+	char* DEBUG__TOLOWER(char* myString)
+		{ int i; for(i=0;myString[i]!='\0';++i){myString[i]=tolower(myString[i]);} return myString; }
 #define TOUPPER(myString)	DEBUG__TOUPPER(myString)
-char* DEBUG__TOUPPER(char* myString)
-{ int i; for(i=0;myString[i]!='\0';++i){myString[i]=toupper(myString[i]);} return myString; }
-
+	char* DEBUG__TOUPPER(char* myString)
+		{ int i; for(i=0;myString[i]!='\0';++i){myString[i]=toupper(myString[i]);} return myString; }
 #else
 #define DEBUGPRINT(...)
 #define DEBUGP(...)
