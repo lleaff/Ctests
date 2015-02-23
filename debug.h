2b9c709e068d0c8df3cc757785c3f13556574ccf
lleaff <lleaff@users.noreply.github.com>
2015-02-24 00:01:42 +0100

    inlined PUTCHARLIT & add TOLOWER & TOUPPER macros

--- debug.h
+++ debug.h
@@ -1,20 +1,32 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 //Put "#define DEBUG" at the top of your file to enable the functions
-#include <stdio.h>
 #ifdef DEBUG
+#include <stdio.h>
+#include <ctype.h>
 #define DEBUGPRINT(...)		do {fprintf(stderr, __VA_ARGS__);}while(0);
 #define DEBUGP(...)			DEBUGPRINT(__VA_ARGS__)
 #define DEBUGSTATEMENT(...)	__VA_ARGS__
 #define DEBUGS(...)	DEBUGSTATEMENT(__VA_ARGS__)
-#define PUTCHARLIT(myChar)	do{Debug__putcharLit(myChar);}while(0);
-void Debug__putcharLit(char myChar);
+
+#define PUTCHARLIT(myChar)	do{switch (myChar) {\
+case '\t':printf("\\t");break;\
+case '\n':printf("\\n");break;\
+case '\0':printf("\\0");break;\
+case ' ':printf("_space_");break;\
+default:putchar(myChar);break;}\
+}while(0);
+
+#define TOLOWER(myString)	do{int i; for(i=0;myString[i]!='\0';++i){tolower(myString[i]);}}while(0);
+#define TOUPPER(myString)	do{int i; for(i=0;myString[i]!='\0';++i){toupper(myString[i]);}}while(0);
+
 #else
 #define DEBUGPRINT(...)
 #define DEBUGP(...)
 #define DEBUGSTATEMENT(...)
 #define DEBUGS(...)
 #define PUTCHARLIT(myChar)
+#define TOLOWER(myString)
 #endif /* DEBUG */
 
 #endif /* DEBUG_H */
